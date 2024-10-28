14a23b1ecb598e57922f9e5d0a947a19e63465d5
ogithaka <ogithaka@gmail.com>
2024-10-28 18:34:30 +0300

    feat: add basic scaffolding for user authentication
    - create placeholder pages for login, register, and reset under (auth) route
    - update layout.module.css to set minimum height for main content area, supporting visual consistency with minimal content
    
    This change establishes a foundation for implementing the full authentication flow and improves layout stability during early development stages.

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,17 @@
+import { Header, Main, NavigationItem } from '@/lib/ui';
+
+export default function Login() {
+    return (
+        <>
+            <Header>
+                <NavigationItem
+                    url=''
+                    name=''
+                />
+            </Header>
+            <Main>
+                <></>
+            </Main>
+        </>
+    );
+}
